d1d9ae7530cf86bc84622a2cca41034ca521c20b
Николай <78875139+NikolayDF@users.noreply.github.com>
2023-11-09 08:30:12 +0300

    Create TwoInputText.vue

---
+++ src/components/elements/TwoInputText.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Минута матча от</p>
+    <q-input filled v-model="text" />
+    <p>до</p>
+    <q-input filled v-model="text" />
+  </div>
+</template>
+
+<script lang="ts">
+
+import {
+  defineComponent,
+  ref
+} from 'vue';
+
+export default defineComponent({
+  name: 'OneInputComponent',
+  /*props: {
+    title: {
+      type: String,
+      default: ''
+    },
+  },*/
+  setup() {
+    return {
+      text: ref('');
+    }
+  }
+});
+</script>
