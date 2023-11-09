d08be4eb33a39a988f702979d8275612736e01b2
Николай <78875139+NikolayDF@users.noreply.github.com>
2023-11-09 08:28:24 +0300

    Create OneInputText.vue

---
+++ src/components/elements/OneInputText.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Счет матча</p>
+    <q-input filled v-model="text" />
+  </div>
+</template>
+
+<script lang="ts">
+
+import {
+  defineComponent,
+  ref
+} from 'vue';
+
+export default defineComponent({
+  name: 'OneInputComponent',
+  /*props: {
+    title: {
+      type: String,
+      default: ''
+    },
+  },*/
+  setup() {
+    return {
+      text: ref('');
+    }
+  }
+});
+</script>
